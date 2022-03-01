5e3b66ff1a846a2b7b93648358a7a2577572a902
ryan luca <58185393+ryan1235@users.noreply.github.com>
2022-03-01 03:15:25 -0300

    sobre mim

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,12 @@ const btnMobileA = document.getElementById('btn-A')
 const btnMobileB = document.getElementById('btn-B')
 const btnMobileC = document.getElementById('btn-C')
 const btnMobileD = document.getElementById('btn-D')
+const btnMobileE = document.getElementById('btn-E')
 
 function togglemenu(){
     const navmobile = document.getElementById('navmobile')
     navmobile.classList.toggle('active')
+    btnMobile.classList.toggle('active')
 }
 
 btnMobile.addEventListener('click', togglemenu)
@@ -14,4 +16,5 @@ btnMobileA.addEventListener('click', togglemenu)
 btnMobileB.addEventListener('click', togglemenu)
 btnMobileC.addEventListener('click', togglemenu)
 btnMobileD.addEventListener('click', togglemenu)
+btnMobileE.addEventListener('click', togglemenu)
  
